01b3ff547267652f2a5fdd19161f1f4981777128
Chico Freitas Mint <chicoo.freitas84@gmail.com>
2023-05-17 17:27:18 -0300

    (feat: ServiceProvider)

---
+++ src/Providers/GettingStartedProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace ChicoFreitas\GettingStarted\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * GettingStartedProvider is the GettingStarted service provider.
+ * 
+ * Why we need a ServiceProvider inside a Laravel package?
+ * 
+ * How is it used for?
+ * 
+ * @package ChicoFreitas\GettingStarted
+ * @author Chico Freitas <chicoo.freitas84@gmail.com
+ * @version 
+ * @see https://github.com/chicofreitas/laravel-getting-started-package
+ */
+class GettingStartedProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     * 
+     * @return void
+     */
+    public function boot()
+    {
+        # code...
+    }
+}
