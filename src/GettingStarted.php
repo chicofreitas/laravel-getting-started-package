6912c4ad677230b798035d803a7ce73c638ce570
Chico Freitas Mint <chicoo.freitas84@gmail.com>
2023-05-17 17:08:48 -0300

    (feat package skeleton)

---
+++ src/GettingStarted.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Defining the class namespace
+ * 
+ * This is made of the vendor name and the package name.
+ */
+namespace ChicoFreitas\GettingStarted;
+
+/**
+ * Class GettingStarted
+ */
+class GettingStarted {
+
+    /**
+     * @return String
+     */
+    public function printSomeMessage()
+    {
+        return 'Doing something in my getting started package!';
+    }
+}
